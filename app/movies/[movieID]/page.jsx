062527394c6d7714388f8d1cd3fa372489c5b1bb
Zainab Alnajjar <eng.zainab.alnajar@gmail.com>
2024-03-30 00:14:52 +0300

    create movie page

--- app/movies/[movieID]/page.jsx
+++ app/movies/[movieID]/page.jsx
@@ -1,17 +1,31 @@
 'use client';
 import React, { useEffect, useState } from 'react'
+import ActorCard from '@/components/ActorCard';
+import CustomSlider from '@/components/CustomSlider';
+import { useRouter } from 'next/navigation';
 
 export default function movie({params}) {
-  // https://api.themoviedb.org/3/movie/movie_id?language=en-US&api_key=ba21689db16b6c3bc58c8f5c53ebd129
+
+  const router = useRouter();
+  async function getTrailer(id){
+    fetch(`https://api.themoviedb.org/3/movie/${params.movieID}/videos?api_key=ba21689db16b6c3bc58c8f5c53ebd129`)
+      .then((res)=> res.json())
+      .then((data) => {
+        router.push(`https://www.youtube.com/watch?v=${data.results[0].key}`)
+      })
+  }
+
   const [movieInfo , setMovieInfo]=useState({
     genres: [
     {
       id: 18,
       name: "Loading"
     },
-  ]});
-  const [crew , setCrew]=useState({ director: "Loading", actors : [] }
-      );
+  ],
+  production_companies:  [{name:""}],
+});
+  const [crew , setCrew]=useState({ director: "Loading", actors : [] });
+  const [similarMovies , setSimilarMovies]=useState([]);
   useEffect(()=>{
     fetch(`https://api.themoviedb.org/3/movie/${params.movieID}?language=en-US&api_key=ba21689db16b6c3bc58c8f5c53ebd129`)
     .then(res => res.json())
@@ -21,52 +35,63 @@ export default function movie({params}) {
     .then(data => {
       const director = data.crew.filter(crew => crew.job === "Director")[0].name;
       const actors = data.cast.slice(0,5);
-      setCrew((prev) => [
-        {
-          ...prev,
-          director: director,
-          actors : [...actors]
-        }
-      ])
-      console.log(actors)
-    })
-    )
+      setCrew({ director: director, actors: [...actors] })
+    })).then(fetch(`https://api.themoviedb.org/3/movie/${params.movieID}/similar?language=en-US&page=1&api_key=ba21689db16b6c3bc58c8f5c53ebd129`)
+    .then(res => res.json())
+    .then(data => setSimilarMovies(data.results)))
   },[])
+
+
   const imagePath = "https://image.tmdb.org/t/p/original"
   const poster = imagePath + movieInfo.poster_path;
-  // const backdrop = imagePath + movieInfo.poster_path;
+  const backdrop = imagePath + movieInfo.backdrop_path;  
 
-
-  //     1. The movie poster
-  //     2. The movie title
-  //     3. Release date   release_date
-  //     4. Runtime    runtime
-  //     5. Movie's Language     original_language
-  //     6. The movie rating and how many votes has it received   vote_average   vote_count
-  //     7. Director's name
-  //     8. Overview of the movie    overview
-  //     9. The main 5 actors of the movies in the credit section (Use the API for this)
-  //     10. A related movies section which includes at least five related movies (Use the API for this)
-  //     11. A trailer section that has the movie trailer from youtube
-  //     12. The movie production company name and logo.  production_companies={[{logo_path,name}]}
-
+  let hours=0;
+  let minutes=movieInfo.runtime;
+  while(minutes >= 60){
+    hours++;
+    minutes-=60;
+  }
   return (
-    <div className='w-full text-white'>
-        <div className="group relative w-full flex flex-row justify-center items-center gap-2">
-          {/* <img src={backdrop} alt="" className="group-hover:blur-sm sm:blur-sm z-[-1] absolute w-full h-full"/> */}
-          <div className="w-[30%] m-auto">
-            <img className='border w-full' src ={poster} alt={movieInfo.title}/>
+    <div className='flex flex-col justify-center items-center'>
+      <div className='relative text-white w-[calc(100%-30px)] mx-auto my-8 flex justify-center items-start flex-col sm:flex-row gap-4 font-serif'>
+        <img src={backdrop} alt={movieInfo.title} className="blur-sm z-[-1] absolute w-full top-0 opacity-50 h-full"/>
+        <div className='w-[80%] min-w-[250px] max-w-[350px] mx-auto md:m-4 rounded-lg border-cyan-400 border-2 overflow-hidden'>
+          <img className='border w-full' src ={poster} alt={movieInfo.title}/>
+        </div>
+        <div className="m-4 sm:max-w-[70%] sm:mx-auto flex flex-col gap-4">
+          <h3 className="text-cyan-500 text-3xl md:text-3xl lg:text-5xl px-8 lg:mt-8 text-center font-serif">{movieInfo.title}</h3>
+          <p className='border-b border-cyan-700 py-4 text-[20px]'><span className='text-cyan-500'>Overview:</span> {movieInfo.overview}</p>
+          <p><span className='text-cyan-500'>Genres:</span> {movieInfo.genres.map(genre => genre.name).join(", ")}</p>
+          <p><span className='text-cyan-500'>Director:</span> {crew.director}</p>
+          <p><span className='text-cyan-500'>Released Date:</span> {movieInfo.release_date}</p>
+          <p><span className='text-cyan-500'>Movie Language:</span> {movieInfo.original_language}</p>
+          <p><span className='text-cyan-500'>Runtime:</span> {hours}h {minutes}m</p>
+          <p><span className='text-cyan-500'>Rate:</span> {movieInfo.vote_average} <i className="fa-solid fa-star text-yellow-300 text-[12px]"></i></p>
+          <p><span className='text-cyan-500'>Votes:</span> {movieInfo.vote_count}</p>
+          <button onClick={() => getTrailer(movie.id)} className="w-fit bg-red-700 hover:bg-red-500 border border-white hover:border-cyan-400 text-white rounded-md p-2 ">Watch Trailer</button>
+          <div><span className='text-cyan-500'>Production Companies: </span> 
+              <div className='flex flex-wrap gap-2'>
+                { movieInfo.production_companies.map(company => {
+                  return (<div key={company.id} className='flex items-center gap-1'><img src={imagePath + company.logo_path} className='w-[30px] max-h-[30px] bg-white bg-opacity-[0.5]'/> <p> {company.name} </p> </div>)
+                  })} 
+              </div>
           </div>
-          <div className="p-2 md:p-0 flex flex-col justify-center items-center w-[40%] text-center">
-            <p className="text-[16px] md:text-[20px] text-cyan-400 font-serif">{movieInfo.title}</p>
-            <p className=" text-white ">{movieInfo.overview}</p>
-            <p className=" text-white ">Genres: {movieInfo.genres.map(genre => genre.name).join(", ")}</p>
-            <p className=" text-white ">Director: {crew.director}</p>
-            {/* <p className="max-sm:hidden text-white ">Actors: {}.</p> */}
-            <div className='flex'>
-            </div>
+
+          <div className='flex'>
+          </div>
+      </div>
+      </div>
+        <div className="w-full mx-auto text-left">
+          <h1 className="text-3xl font-bold pl-4 mb-4 py-4 text-cyan-400 border-b border-cyan-900">Main Actors</h1>
+          <div className="flex flex-wrap justify-center gap-4">
+            <ActorCard actors={crew.actors} />
+          </div>
+          <div className='my-4'>
+          <h1 className="text-3xl font-bold pl-4 mb-4 py-4 text-cyan-400 border-b border-cyan-900">Similar Movies</h1>
+          <CustomSlider Movies={similarMovies}/>
           </div>
         </div>
-      </div>
+    </div>
   )
 }
