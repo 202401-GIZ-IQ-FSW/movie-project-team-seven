64248fe62a17b80a77ba9875c09a527ee7a3b582
Zainab Alnajjar <eng.zainab.alnajar@gmail.com>
2024-04-20 15:06:26 +0300

    fix key warning

--- components/SectionSkeleton.js
+++ components/SectionSkeleton.js
@@ -6,13 +6,11 @@ export default function SectionSkeleton() {
     <div>
       <p className="text-center text-[24px] font-serif border-b border-cyan-900 m-4 text-cyan-500">Loading...</p>
       <div className='m-[16px] flex flex-wrap justify-center items-center gap-6'>
-        {[...Array(20)].map((index)=> (
-            <div key={index} className="animate-pulse bg-slate-600 h-[200px] w-[150px] flex justify-center items-center border border-cyan-900">
+        {[...Array(20)].map(()=> (
+            <div key={Math.random()} className="animate-pulse bg-slate-600 h-[200px] w-[150px] flex justify-center items-center border border-cyan-900">
                 <Image
                   quality={100}
                   unoptimized={true}
-                  // priority={true}
-                  // loading='eager'
                   width={"100"} height={"100"}
                   className='w-full ' 
                   src="/images/Phoenix-logo.png" alt='logo'/>
