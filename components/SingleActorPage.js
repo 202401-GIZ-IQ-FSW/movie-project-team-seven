924a297c72ed6099703d42616245885f199b8f64
Zainab Alnajjar <eng.zainab.alnajar@gmail.com>
2024-04-20 17:16:22 +0300

    add placeholders

--- components/SingleActorPage.js
+++ components/SingleActorPage.js
@@ -21,19 +21,24 @@ export default async function SingleActorPage({id}) {
                         className='w-full h-full rounded-lg border-cyan-400 border-2'
                         id='actorImage'
                     />
+                    {actorInfo.popularity? 
                     <p className='mt-4'><span className='text-cyan-500'> Popularity:</span> {actorInfo.popularity}</p>
+                    :""}
                 </div>
-                <div className='max-sm:m-4 sm:max-w-[70%] sm:mx-auto flex flex-col gap-4'>
+                <div className='max-sm:m-4 sm:w-[70%] sm:mx-auto flex flex-col gap-4'>
                     <h3 className="text-cyan-500 text-3xl md:text-3xl lg:text-6xl px-8 lg:mt-8 text-center">{actorInfo.name}</h3>
-                    <h4 className='text-[15px] md:text-2xl lg:text-[20px] text-center'><span className='text-cyan-500'>Birthday:</span> {actorInfo.birthday}</h4>  {/* Birth Day */}
-                    <p className=" leading-loose"><span className='text-cyan-500'>Biography:</span> {actorInfo.biography}</p> {/* Biography */}
+                    {actorInfo.birthday?
+                    <h4 className='text-[15px] md:text-2xl lg:text-[20px] text-center'><span className='text-cyan-500'>Birthday:</span> {actorInfo.birthday}</h4>
+                    :""
+                    }
+                    <p className=" leading-loose"><span className='text-cyan-500'>Biography:</span> {actorInfo.biography? actorInfo.biography: "N/A"}</p>
                     <div className='max-sm:text-[12px]'>
-                        <p><span className='text-cyan-500'> Place of birth:</span> {actorInfo.place_of_birth}</p>
+                        <p><span className='text-cyan-500'> Place of birth:</span> {actorInfo.place_of_birth? actorInfo.place_of_birth: "N/A"}</p>
                         <p><span className='text-cyan-500'> Gender:</span> {actorInfo.gender ==1? "Female" : "Male"}</p>
                     </div>
                 </div>
             </div>
             <p className="text-[24px] font-serif border-b border-cyan-900 m-4 text-cyan-500">Participated in:</p>
-            <CustomSlider Movies={actorInfo.movie_credits?.cast}/>
+            {(actorInfo.movie_credits.cast.length > 0)? <CustomSlider Movies={actorInfo.movie_credits.cast}/> : <p className='text-cyan-400 m-8 text-center'>No movies provided from the API</p>}
         </div>)
 }
